068dc92d0764d2d938c77b843e8e12a39fe41b82
Hannah <108206521+ayemyatlwin@users.noreply.github.com>
2024-05-28 23:10:20 +0630

    Navbar added

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import Navbar from "./Navbar";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div style={{ backgroundColor: "#f5f5f5", height: "90vh" }}>
+        {children}
+      </div>
+    </div>
+  );
+}
