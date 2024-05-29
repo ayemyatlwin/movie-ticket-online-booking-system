ab7fc8dd55ac78cb9c6eb2ca92ad7d874471f8ff
Hannah <108206521+ayemyatlwin@users.noreply.github.com>
2024-05-29 14:38:43 +0630

    image path change

--- src/app/cinemas/page.tsx
+++ src/app/cinemas/page.tsx
@@ -28,7 +28,6 @@ const CinemaListPage = () => {
     selectedCinemaRoom,
     selectedShowDate,
     selectedShowTime,
-    selectedSeat,
   } = useSelector((state: RootState) => state.TicketSlice);
   console.log("selectedCinemaRoom", selectedCinemaRoom);
 
