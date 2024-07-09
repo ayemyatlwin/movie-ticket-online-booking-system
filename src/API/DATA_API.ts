fb8d6e5f95b169fcf05858e64c836ad7dbf76a6d
Hannah <108206521+ayemyatlwin@users.noreply.github.com>
2024-07-09 19:43:47 +0630

    file name error fixed

--- src/API/DATA_API.ts
+++ src/API/DATA_API.ts
@@ -2,7 +2,7 @@ import { JSON_URL } from "@/config/apiConfig";
 import axios from "axios";
 
 const DATA_API = axios.create({
-  baseURL: JSON_URL,
+   baseURL: JSON_URL,
 });
 
 export default DATA_API;
